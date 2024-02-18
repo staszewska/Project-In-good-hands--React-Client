69e2cca83732d380cc0ab5dd39367a893d1830eb
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-02-18 23:38:46 +0100

    header component created and css added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+import heroImage from "../assets/Home-Hero-Image.jpg";
+
 const Header = () => {
   return (
     <div className="header">
-      <p>Zacznij pomagać!</p>
-      <p>Oddaj niechciane rzeczy w zaufane ręce</p>
+      <div className="right-header">
+        <img src={heroImage} alt="Hero" style={{ width: "700px" }}></img>
+      </div>
+      <div className="left-header">
+        <p>Zacznij pomagać!</p>
+        <p>Oddaj niechciane rzeczy w zaufane ręce</p>
+        <Decoration />
+        <button>ODDAJ RZECZY</button>
+        <button>ZORGANIZUJ ZBIÓRKĘ</button>
+      </div>
     </div>
   );
 };
