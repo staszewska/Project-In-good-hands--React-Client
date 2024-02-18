fa89413c1f0f7b7d531ee466848634cb78bff865
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-02-18 23:37:36 +0100

    navbar div fixed an css added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,12 +7,13 @@ const NavBar = () => {
         <Link to="login">Zaloguj sie</Link>
         <Link to="register">Zarejestruj sie</Link>
       </div>
-      <div className="links"></div>
-      <Link to="/">Start</Link>
-      <Link to="about">O co chodzi?</Link>
-      <Link to="us">O nas</Link>
-      <Link to="foundation">Fundacja i organizacja</Link>
-      <Link to="contact">Kontakt</Link>
+      <div className="links">
+        <Link to="/">Start</Link>
+        <Link to="about">O co chodzi?</Link>
+        <Link to="us">O nas</Link>
+        <Link to="foundation">Fundacja i organizacja</Link>
+        <Link to="contact">Kontakt</Link>
+      </div>
     </nav>
   );
 };
