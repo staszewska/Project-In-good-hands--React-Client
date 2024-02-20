864a531c261267939a4e32e784f925d2d66cc37d
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-02-20 13:16:02 +0100

    FourSteps component created and styled

---
+++ src/components/FourSteps.js
@@ -0,0 +1,14 @@
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+
+const FourSteps = () => {
+  return (
+    <div className="FourStepsContainer">
+      <div className="FourStepsContainer__Header">
+        <p>Wystarczą 4 proste kroki</p>
+        <Decoration />
+      </div>
+    </div>
+  );
+};
+
+export default FourSteps;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import NavBar from "./NavBar";
 import Header from "./Header";
 import ThreeColumns from "./ThreeColumns";
+import FourSteps from "./FourSteps";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <NavBar />
       <Header />
       <ThreeColumns />
+      <FourSteps />
     </div>
   );
 };
