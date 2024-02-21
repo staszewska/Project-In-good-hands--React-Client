c7bc60b2e5a5db938a33b3f7f343d81307aabd66
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-02-21 15:18:03 +0100

    Help component initiated

---
+++ src/components/Help.js
@@ -0,0 +1,15 @@
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+
+const Help = () => {
+  return (
+    <div className="HelpForm">
+      <h1>Komu pomagamy?</h1>
+      <Decoration />
+      <div className="HelpForm__Buttons">
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default Help;
