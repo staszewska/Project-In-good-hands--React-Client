065272ea2cf356bdbe8a06074144beec422540be
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-02-22 19:41:08 +0100

    basic css for HelpForm

--- src/components/Help.js
+++ src/components/Help.js
@@ -8,34 +8,43 @@ const Help = () => {
       "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy.",
     name1: 'Fundacja "Dbam o zdrowie"',
     para: "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej",
+    things: "ubrania, jedzenie, meble, sprzęt AGD, zabawki",
     name2: 'Fundacja "Dla dzieci"',
     para2: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+    things2: "ubrania, meble, zabawki",
     name3: 'Fundacja "Bez domu"',
     para3:
       "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+    things3: "ubrania, jedzenie, ciepłe koce",
   };
 
   const obj2 = {
     description:
       "Officia excepteur ut velit velit excepteur amet officia occaecat veniam magna eu ad laborum. Enim deserunt ad aliqua occaecat in cupidatat pariatur Lorem consectetur nisi esse do. Ad est aliqua anim eiusmod dolore ipsum veniam. Officia proident amet id non elit. Consequat id quis cillum eiusmod laboris est mollit enim duis cillum dolor labore nostrud anim. Consectetur mollit ipsum aliquip fugiat cupidatat culpa.",
-    name1: 'Organizacja "Lorem Ipsum1',
+    name1: 'Organizacja "Lorem Ipsum1"',
     para: "Aliquip occaecat do eu qui officia esse magna laboris incididunt. Quis reprehenderit fugiat reprehenderit cillum do pariatur ex. Incididunt adipisicing aute dolore adipisicing ipsum excepteur velit cupidatat culpa. Magna cupidatat sint qui reprehenderit veniam mollit exercitation magna elit veniam do labore.",
-    name2: 'Organizacja "Lorem Ipsum2',
+    things: "Sit, officia, pariatur",
+    name2: 'Organizacja "Lorem Ipsum2"',
     para2: "Esse ullamco ipsum officia esse.",
-    name3: 'Organizacja "Lorem Ipsum3',
+    things2: "Sit, adipisicing, mollit",
+    name3: 'Organizacja "Lorem Ipsum3"',
     para3: "Proident id adipisicing eu qui ut fugiat qui labore.",
+    things3: "Fugiat, esse, cillum",
   };
 
   const obj3 = {
     description:
       "Mollit et quis enim laboris do elit. Minim fugiat minim esse laboris cupidatat do. Veniam cillum proident magna incididunt voluptate ipsum ad pariatur. Voluptate sunt id ex amet nostrud aliqua ex. Cillum tempor irure fugiat laboris officia adipisicing adipisicing enim ullamco deserunt dolor. Nulla amet reprehenderit voluptate officia incididunt cillum amet sit aute eiusmod proident elit consectetur.",
-    name1: 'Zbiórka "Lorem Ipsum1',
+    name1: 'Zbiórka "Lorem Ipsum1"',
     para: "Pariatur exercitation non proident laboris mollit labore.",
-    name2: 'Zbiórka "Lorem Ipsum2',
+    things: "Fugiat, esse, cillum",
+    name2: 'Zbiórka "Lorem Ipsum2"',
     para2:
       "Quis commodo elit consequat labore deserunt nostrud tempor nisi voluptate consequat do.",
-    name3: 'Zbiórka "Lorem Ipsum3',
+    things2: "Sit, officia, pariatur",
+    name3: 'Zbiórka "Lorem Ipsum3"',
     para3: "Occaecat laboris proident duis dolor magna culpa officia in ea.",
+    thing3s: "Sit, officia, pariatur",
   };
 
   const [selectedObj, setSelectedObj] = useState(obj1);
@@ -49,11 +58,22 @@ const Help = () => {
       <h1>Komu pomagamy?</h1>
       <Decoration />
       <div className="HelpForm__Buttons">
-        <button onClick={() => handleButtonClick(obj1)}>Fundacjom</button>
-        <button onClick={() => handleButtonClick(obj2)}>
+        <button
+          className="HelpForm__Button"
+          onClick={() => handleButtonClick(obj1)}
+        >
+          Fundacjom
+        </button>
+        <button
+          className="HelpForm__Button"
+          onClick={() => handleButtonClick(obj2)}
+        >
           Organizacjom pozarządowym
         </button>
-        <button onClick={() => handleButtonClick(obj3)}>
+        <button
+          className="HelpForm__Button"
+          onClick={() => handleButtonClick(obj3)}
+        >
           Lokalnym zbiórkom
         </button>
       </div>
--- src/components/HelpForm.js
+++ src/components/HelpForm.js
@@ -1,21 +1,49 @@
 const HelpForm = ({ data }) => {
-  const { description, name1, name2, name3, para, para2, para3 } = data;
+  const {
+    description,
+    name1,
+    name2,
+    name3,
+    para,
+    para2,
+    para3,
+    things,
+    things2,
+    things3,
+  } = data;
   // const description = props.data.description
 
   return (
-    <div>
-      <p>{description}</p>
-      <div>
-        <p>{name1}</p>
-        <p>{para}</p>
-      </div>
-      <div>
-        <p>{name2}</p>
-        <p>{para2}</p>
-      </div>
-      <div>
-        <p>{name3}</p>
-        <p>{para3}</p>
+    <div className="HelpForm__Container">
+      <p className="HelpForm__Description">{description}</p>
+
+      <div className="HelpForm__Content">
+        <div>
+          <div>
+            <p>{name1}</p>
+            <p>{para}</p>
+          </div>
+
+          <p>{things}</p>
+        </div>
+
+        <div>
+          <div>
+            <p>{name2}</p>
+            <p>{para2}</p>
+          </div>
+          <p>{things2}</p>
+        </div>
+        <div>
+          <div>
+            <p>{name3}</p>
+            <p>{para3}</p>
+          </div>
+
+          <div>
+            <p>{things3}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
