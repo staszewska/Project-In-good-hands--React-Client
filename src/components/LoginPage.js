11e036185821b48888c70cffb120df58ff70a92d
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-02-27 18:28:37 +0100

    LoginPage header styled

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,5 +1,18 @@
+import NavBar from "./NavBar";
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+
 const LoginPage = () => {
-  return <h1>Login component</h1>;
+  return (
+    <>
+      <NavBar />
+      <div className="LoginPageContainer">
+        <div className="LoginPage__Header">
+          Zaloguj się
+          <Decoration />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
