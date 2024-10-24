2cfe270e55bcc743aaf30b5524da2d73057b9a48
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-10-24 14:10:12 +0200

    translate to english

--- src/components/Help.js
+++ src/components/Help.js
@@ -5,102 +5,102 @@ import HelpForm from "./HelpForm";
 const Help = () => {
   const obj1 = {
     description:
-      "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy.",
+      "In our database, you will find a list of verified foundations we collaborate with.",
     items: [
       {
         id: 1,
-        name: '1. Fundacja "Dbam o zdrowie"',
-        para: "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej",
-        things: "ub}rania, jedzenie, meble, zabawki",
+        name: '1. Foundation "I Care About Health"',
+        para: "Goal and mission: Helping people in difficult life situations",
+        things: "clothes, food, furniture, toys",
       },
       {
         id: 2,
-        name: '2. Fundacja "Dla dzieci"',
-        para: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
-        things: "ubrania, meble, zabawki",
+        name: '2. Foundation "For Children"',
+        para: "Goal and mission: Helping children from low-income families.",
+        things: "clothes, furniture, toys",
       },
       {
         id: 3,
-        name: '3. Fundacja "Bez domu"',
-        para: "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
-        things: "ubrania, jedzenie, ciepłe koce",
+        name: '3. Foundation "Without a Home"',
+        para: "Goal and mission: Helping people without a place to live.",
+        things: "clothes, food, warm blankets",
       },
       {
         id: 4,
-        name: '4. Fundacja "Dla dzieci"',
-        para: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
-        things: "ubrania, meble, zabawki",
+        name: '4. Foundation "For Children"',
+        para: "Goal and mission: Helping children from low-income families.",
+        things: "clothes, furniture, toys",
       },
       {
         id: 5,
-        name: '5. Fundacja "Bez domu"',
-        para: "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
-        things: "ubrania, jedzenie, ciepłe koce",
+        name: '5. Foundation "Without a Home"',
+        para: "Goal and mission: Helping people without a place to live.",
+        things: "clothes, food, warm blankets",
       },
       {
         id: 6,
-        name: '6. Fundacja "Dla dzieci"',
-        para: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
-        things: "ubrania, meble, zabawki",
+        name: '6. Foundation "For Children"',
+        para: "Goal and mission: Helping children from low-income families.",
+        things: "clothes, furniture, toys",
       },
       {
         id: 7,
-        name: '7. Fundacja "Bez domu"',
-        para: "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
-        things: "ubrania, jedzenie, ciepłe koce",
+        name: '7. Foundation "Without a Home"',
+        para: "Goal and mission: Helping people without a place to live.",
+        things: "clothes, food, warm blankets",
       },
       {
         id: 8,
-        name: '8. Fundacja "Dla dzieci"',
-        para: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
-        things: "ubrania, meble, zabawki",
+        name: '8. Foundation "For Children"',
+        para: "Goal and mission: Helping children from low-income families.",
+        things: "clothes, furniture, toys",
       },
       {
         id: 9,
-        name: '9. Fundacja "Bez domu"',
-        para: "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
-        things: "ubrania, jedzenie, ciepłe koce",
+        name: '9. Foundation "Without a Home"',
+        para: "Goal and mission: Helping people without a place to live.",
+        things: "clothes, food, warm blankets",
       },
     ],
   };
 
   const obj2 = {
     description:
-      "Officia excepteur ut velit velit excepteur amet officia occaecat veniam magna eu ad laborum. ",
+      "Official excepteur ut velit velit excepteur amet officia occaecat veniam magna eu ad laborum.",
     items: [
       {
         id: 1,
-        name: '1. Organizacja "Lorem Ipsum1"',
+        name: '1. Organization "Lorem Ipsum1"',
         para: "Aliquip occaecat do eu qui officia esse magna laboris incididunt. Quis reprehenderit fugiat reprehenderit cillum do pariatur ex.",
         things: "Sit, officia, pariatur",
       },
       {
         id: 2,
-        name: '2. Organizacja "Lorem Ipsum2"',
+        name: '2. Organization "Lorem Ipsum2"',
         para: "Esse ullamco ipsum officia esse.",
         things: "Sit, adipisicing, mollit",
       },
       {
         id: 3,
-        name: '3. Organizacja "Lorem Ipsum3"',
+        name: '3. Organization "Lorem Ipsum3"',
         para: "Proident id adipisicing eu qui ut fugiat qui labore.",
         things: "Fugiat, esse, cillum",
       },
       {
         id: 4,
-        name: '4. Organizacja "Lorem Ipsum1"',
+        name: '4. Organization "Lorem Ipsum1"',
         para: "Aliquip occaecat do eu qui officia esse magna laboris incididunt. Quis reprehenderit fugiat reprehenderit cillum do pariatur ex.",
         things: "Sit, officia, pariatur",
       },
       {
         id: 5,
-        name: '5. Organizacja "Lorem Ipsum2"',
+        name: '5. Organization "Lorem Ipsum2"',
         para: "Esse ullamco ipsum officia esse.",
         things: "Sit, adipisicing, mollit",
       },
       {
         id: 6,
-        name: '6. Organizacja "Lorem Ipsum3"',
+        name: '6. Organization "Lorem Ipsum3"',
         para: "Proident id adipisicing eu qui ut fugiat qui labore.",
         things: "Fugiat, esse, cillum",
       },
@@ -113,19 +113,19 @@ const Help = () => {
     items: [
       {
         id: 1,
-        name: '1. Zbiórka "Lorem Ipsum1"',
+        name: '1. Collection "Lorem Ipsum1"',
         para: "Pariatur exercitation non proident laboris mollit labore.",
         things: "Fugiat, esse, cillum",
       },
       {
         id: 2,
-        name: '2. Zbiórka "Lorem Ipsum2"',
+        name: '2. Collection "Lorem Ipsum2"',
         para: "Quis commodo elit consequat labore deserunt nostrud tempor nisi voluptate consequat do.",
         things: "Sit, officia, pariatur",
       },
       {
         id: 3,
-        name: '3. Zbiórka "Lorem Ipsum3"',
+        name: '3. Collection "Lorem Ipsum3"',
         para: "Occaecat laboris proident duis dolor magna culpa officia in ea.",
         things: "Sit, officia, pariatur",
       },
@@ -140,26 +140,26 @@ const Help = () => {
 
   return (
     <div className="HelpForm">
-      <h1>Komu pomagamy?</h1>
+      <h1>Who do we help?</h1>
       <Decoration />
       <div className="HelpForm__Buttons">
         <button
           className="HelpForm__Button"
           onClick={() => handleButtonClick(obj1)}
         >
-          Fundacjom
+          Foundations
         </button>
         <button
           className="HelpForm__Button"
           onClick={() => handleButtonClick(obj2)}
         >
-          Organizacjom pozarządowym
+          Non-Governmental Organizations
         </button>
         <button
           className="HelpForm__Button"
           onClick={() => handleButtonClick(obj3)}
         >
-          Lokalnym zbiórkom
+          Local Collections
         </button>
       </div>
 
