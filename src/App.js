22608216c34d23a861b2b25b3a8bc2638a5891d6
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-11-20 21:26:31 +0100

    set state for userData and pass props to LoginPage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import LoginPage from "./components/LoginPage";
@@ -6,11 +6,22 @@ import AboutUs from "./components/AboutUs";
 import RegisterPage from "./components/RegisterPage";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  function handleUserLogIn(userData) {
+    setUserData(userData);
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" exact element={<Home userData={userData} />} />
+        <Route
+          path="/login"
+          element={
+            <LoginPage onUserLogIn={(userData) => handleUserLogIn(userData)} />
+          }
+        />
         <Route path="/register" element={<RegisterPage />} />
         {/* <Route path="/us" element={<UsPage />} />
         <Route path="/foundation" element={<FoundationPage />} />
